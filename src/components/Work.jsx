ed0ee6b62e89bb24f725712fb85e78e7295887fd
kittiporn <kittipornbanky@gmail.com>
2022-03-24 17:13:36 +0700

    change work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -26,7 +26,7 @@ const Work = () => {
                 React JS Application
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a href="https://mern-stack-kittiporn.herokuapp.com/" target="_blank">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
